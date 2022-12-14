2686f4323d32552c8f7ccd1725395475af560898
Speed431 <73215393+Speed431@users.noreply.github.com>
2022-12-14 17:07:36 +0700

    style css updated

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-document.querySelector('.calc__btn').addEventListener('click', e => console.log(e.target))
+// document.querySelector('.calc__btn').addEventListener('click', e => console.log(e.target))
+
+
+let val1 = 0;
+let val2 = 0;
+
+document.querySelector('.calc__btn').addEventListener('click', (e) => {
+    console.log(e.target.value)
+})
