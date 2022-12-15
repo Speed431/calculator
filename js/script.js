9c35dc6971823a3902a7ffd5515bbb45f386788c
Speed431 <73215393+Speed431@users.noreply.github.com>
2022-12-15 07:10:03 +0700

    added funcion print to click buttons

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-// document.querySelector('.calc__btn').addEventListener('click', e => console.log(e.target))
-
-
-let val1 = 0;
+let val1 = '';
 let val2 = 0;
+thisVal = true;
+const input = document.querySelector('.calc__conclusion > input');
 
 document.querySelector('.calc__btn').addEventListener('click', (e) => {
-    console.log(e.target.value)
+    if (e.srcElement.offsetParent.classList.contains('number')) {
+        val1 += e.target.outerText;
+    }
+    input.value = val1;
 })
+
+
+function changeValue() {
+    if (thisVal) {
+        val1 = input.value;
+    }
+}
